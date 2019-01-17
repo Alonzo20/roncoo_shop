89c590a8a559cacb2838072139149e68c86ac759
yezhimin <976855420@qq.com>
2019-01-17 15:17:02 +0800

    Spring Cloud之Hystrix熔断降级

--- say-hello-service/src/main/java/com/roncoo/eshop/SayHelloServiceApplication.java
+++ say-hello-service/src/main/java/com/roncoo/eshop/SayHelloServiceApplication.java
@@ -3,13 +3,19 @@ package com.roncoo.eshop;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class SayHelloServiceApplication {
 
 	public static void main(String[] args) {
